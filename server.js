4ee1b96332b22dfd9d37fda88c3c7fb7ff642601
afnlite <afnlite@gmail.com>
2018-02-24 07:48:56 +0200

    Install opn and create basic express server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var opn = require('opn');
+
+var port = 3030;
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+   if (err) {
+       console.error(err);
+   } else {
+     opn('http://localhost:' + port);
+    }
+});
+
+

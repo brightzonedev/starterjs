b690f297248c58203622e98d6e5c95e2141b3c9a
afnlite <afnlite@gmail.com>
2018-02-24 08:02:00 +0200

    Create src directory to be served by server and fix src path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = 3030;
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 app.listen(port, function(err) {
